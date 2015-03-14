396299f9cc8ab239b705c18a1db88c9b04552ed5
ahtml <anandhtml@gmail.com>
2015-03-14 15:58:35 -0400

    Added StraigthWE subclass of Path. All of the path subclasses use heading of compass as part of the naming scheme. The first letter relates to where the critter will enter and the second letter is where the critter will exit. For example, for StraightWE, the first letter W shows that the critter will enter from the West and the second letter E shows that the critter will exit from the East.

---
+++ src/StraightWE.java
@@ -0,0 +1,17 @@
+
+public class StraightWE extends Path implements IPathActions {
+	
+	public StraightWE(int pos, int width){
+		super(pos,width);
+		setParameters(pos);
+	}
+
+	public void setParameters(int pos) {
+		
+		this.pos = pos;
+		ent = pos-1; // calculate entrance
+		exit = pos+1; // Calculate exit
+		
+	}
+
+}
