0b22c824d3f286abda2ad249d06267c1150d4543
ahtml <anandhtml@gmail.com>
2015-03-14 15:41:58 -0400

    Added Path.java. Methods are similar to the ones found in Path subcalsses found in Anand's assignment 1. The methods to add into the class were determined through the commenting scheme found in IPathActions interface.

---
+++ src/Path.java
@@ -0,0 +1,63 @@
+
+public class Path extends Tile{
+	
+	// Will try them as public first then see if can change the variables to private
+	public int ent; // Tile from where critter will enter
+	public int exit; // Till which the critter will exit
+	public int width; // width of the Map Used to calculate exit/entrance of tile
+	public boolean isEdge; // If this tile is an entrance or exit or if it is part of path
+	public boolean edgeType; // If it is an entrance or exit
+	public boolean visited; // Check if the tile was visited during verification process
+	
+	public Path(int pos,int width){
+		this.pos = pos;
+		this.width = width;
+	}
+	
+	public void rotate() { // Rotate piece
+		int temp = ent;
+		ent = exit;
+		exit = temp; 
+	}
+	
+	public int getEntrance(){ // return entrance of tile
+		return ent;
+	}
+	
+	public int getExit(){ // return exit of tile
+		return exit;
+	}
+	
+	public void setisEdge(){ //setting that there it may be either an entrance or exit
+		isEdge = true;
+	}
+	
+	public boolean getisEdge(){ //know if its a regular path or either entrance or exit
+		return isEdge;
+	}
+	
+	public void setEntry(){ //setting tile as entry point
+		setisEdge();
+		edgeType = true;
+		ent = pos;
+	}
+	
+	public void setExit(){ //setting tile as exit point
+		setisEdge();
+		edgeType = false;
+		exit = pos;
+	}
+	
+	public boolean getedgeType(){ // getting the boolean which denotes if entry or exit point
+		return edgeType;
+	}
+	
+	public void setVisited(boolean visited){
+		this.visited = visited;
+	}
+	
+	public boolean getVisited(){
+		return visited;
+	}
+
+}
