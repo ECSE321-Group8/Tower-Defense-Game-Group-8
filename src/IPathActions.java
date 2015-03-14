55a150ad210527673ff69e37e3612076c04dc257
ahtml <anandhtml@gmail.com>
2015-03-14 15:22:54 -0400

    These are the methods which are unique to each subclass of Path(still to be created). More mehtod signatures will be added later.

---
+++ src/IPathActions.java
@@ -0,0 +1,18 @@
+/*
+ * All comments with 2 slashes are general to the Path
+ * All comments with 4 slashes are general to the Tile
+ */
+
+public interface IPathActions {
+	// public void rotate();
+	// public int getEntrance();
+	// public int getExit();
+	// // public int getpos();
+	// public void setisEdge();
+	// public boolean getisEdge();
+	// public void setEntry();
+	// public void setExit();
+	// public boolean getedgeType();
+	// public void setVisited(boolean visited);
+	public void setParameters(int pos);
+}
