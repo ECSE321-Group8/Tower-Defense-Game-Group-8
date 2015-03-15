db18f8d5c270f123b17b5da6acb72269fb74b7f9
aimee-ascencio <aimee_16_94@hotmail.com>
2015-03-15 11:51:19 -0400

    updates to class Scenery

--- src/Scenery.java
+++ src/Scenery.java
@@ -9,17 +9,40 @@ public class Scenery extends Tile{
 		towerPresent=false;
 	}
 	
+	/*
+	 * Returns true if there is a tower present the Tile Path
+	 */
 	public boolean isTowerPresent(){
 		return towerPresent;
 	}
 	
+	
+	/*
+	 * If it is able to place a tower on a tile Path it returns true
+	 * It is only able to place a tower, if there are no towers present in that tile
+	 */
 	public boolean towerPlaced(){
 		if(!isTowerPresent()){//if there are no towers 
 			towerPresent=true;// a tower is placed 
-			return true;
+			return true;//tower was successfully added
 		}
 		else//there is already a tower there 
-			return false;
+			return false;//can't add another tower
 	}	
-
+	
+	/*
+	 * 
+	 */
+	public boolean towerRemoved(){
+		if (isTowerPresent()){//if there is a tower
+			towerPresent=false;//remove tower
+			return true;//tower was successfully removed 
+		}
+		else 
+			return false;//there is no tower to remove
+	}
+	
+	
+	
+	
 }
