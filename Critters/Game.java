dea9b0cd54e10c4ec80bc028dc9fcfb89f2e8cda
olivertsesk <olivertsesk@hotmail.com>
2015-03-15 15:43:52 -0400

    Create Game.java

---
+++ Critters/Game.java
@@ -0,0 +1,19 @@
+
+public class Game implements Iupdate{
+		private int money;
+		private int life;
+		
+		public Game()
+		{
+			this.money=1000;
+			this.life=10;
+		}
+		public void updatelife()
+		{
+			life=life-critter.gettype();
+		}
+		public void updatemoney()
+		{
+			this.money=this.money+critter.gettype();
+		}
+}
