d34f4d1b0bdfc07f14132ee74d8c13f3354551a4
ahtml <anandhtml@gmail.com>
2015-03-15 17:31:32 -0400

    Tested for all cases for invalid spots in top right.

--- src/PathPiecesConnected.java
+++ src/PathPiecesConnected.java
@@ -53,14 +53,14 @@ public class PathPiecesConnected {
 	public void test() {
 		
 		/*
-		 *  Case 1: See if can recognize out of bounds from top left corner
+		 *  Case 1: See if can recognize expecting out of bounds from top left corner
 		 *   a) For StriaightWE
 		 *   b) For StraightNS
 		 *   c) For TurnEN
 		 *   d) For TurnNW (Should be true)
 		 *   e) For TurnSE
 		 *   f) For TurnWS
-		 *   Each one of these has to be repeated for a rotate form of the path as well
+		 *   Each one of these has to be repeated for a rotated form of the path as well
 		 */
 
 		assertFalse("Case 1-a: did not work.",m.inValidSpot(p1));
@@ -77,6 +77,50 @@ public class PathPiecesConnected {
 		assertFalse("Case 1-f(r): did not work.",m.inValidSpot(p12));
 		// worked
 		
+		/*
+		 *  Case 2: See if can recognize expecting out of bounds from top right corner
+		 *   a) For StriaightWE
+		 *   b) For StraightNS
+		 *   c) For TurnEN
+		 *   d) For TurnNW
+		 *   e) For TurnSE
+		 *   f) For TurnWS (Should be true)
+		 *   Each one of these has to be repeated for a rotated form of the path as well
+		 */
+		
+		// Position 9 is the top right of a 10x10 grid
+		p1 = new StraightWE(9,10);
+		p2 = new StraightWE(9,10);
+		p2.rotate();
+		p3 = new StraightNS(9,10);
+		p4 = new StraightNS(9,10);
+		p4.rotate();
+		p5 = new TurnEN(9,10);
+		p6 = new TurnEN(9,10);
+		p6.rotate();
+		p7 = new TurnNW(9,10);
+		p8 = new TurnNW(9,10);
+		p8.rotate();
+		p9 = new TurnSE(9,10);
+		p10 = new TurnSE(9,10);
+		p10.rotate();
+		p11 = new TurnWS(9,10);
+		p12 = new TurnWS(9,10);
+		p12.rotate();
+		
+		assertFalse("Case 1-a: did not work.",m.inValidSpot(p1));
+		assertFalse("Case 1-a(r): did not work.",m.inValidSpot(p2));
+		assertFalse("Case 1-b: did not work.",m.inValidSpot(p3));
+		assertFalse("Case 1-b(r): did not work.", m.inValidSpot(p4));
+		assertFalse("Case 1-c: did not work.",m.inValidSpot(p5));
+		assertFalse("Case 1-c(r): did not work.",m.inValidSpot(p6));
+		assertFalse("Case 1-d: did not work.",m.inValidSpot(p7));
+		assertFalse("Case 1-d(r): did not work.",m.inValidSpot(p8));
+		assertFalse("Case 1-e: did not work.",m.inValidSpot(p9));
+		assertFalse("Case 1-e(r): did not work.", m.inValidSpot(p10));
+		assertTrue("Case 1-f: did not work.", m.inValidSpot(p11));
+		assertTrue("Case 1-f(r): did not work.",m.inValidSpot(p12));
+		//worked
 		
 		
 	}
