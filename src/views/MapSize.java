b5994f9db2f095cb85518e73bfb759cb4d0d15ca
ahtml <anandhtml@gmail.com>
2015-03-15 20:07:39 -0400

    Starting putting together Map Size

--- src/views/MapSize.java
+++ src/views/MapSize.java
@@ -1,5 +1,28 @@
 package views;
 
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
 public class MapSize {
+	
+	JFrame frame = new JFrame ();
+	public static final int SCREEN_WIDTH = 100;
+	public static final int SCREEN_HEIGHT = 100;
+	public static final String APP_NAME = "SIZE OF MAP";
+	
+	public int sizeRows;
+	public int sizeColumns;
+	public JTextField textRows;
+	public JTextField textColumns;
+	
+	public MapSize () {
+		
+	}
+	
+	private void init(){
+		
+		frame.add(textRows);
+		frame.add(textColumns);;
+	}
 
 }
