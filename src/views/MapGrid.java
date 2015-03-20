e73f073e606b18ba5d2a30a4466a58cc4e54d6e0
aimee-ascencio <aimee_16_94@hotmail.com>
2015-03-20 07:41:28 -0400

    changed imports

--- src/views/MapGrid.java
+++ src/views/MapGrid.java
@@ -1,12 +1,7 @@
 package views;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.*;
+import java.awt.event.*;
 
 import javax.swing.JPanel;
 
